760f9c934f1e91a77f84bcdfff02aba1d1a61a78
Your Name <you@example.com>
2020-05-14 13:17:54 +0300

    summa

---
+++ Summ.pas
@@ -0,0 +1,11 @@
+﻿var 
+  a,b,c,:integer;
+  begin
+    writeln('Введите 1 число ');
+    readln(a);
+    writeln('Введите 1 число ');
+    readln(b);
+    c:=a+b;
+    writeln('Сумма = ',c);
+  
+  end.
