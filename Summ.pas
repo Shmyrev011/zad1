6580af7884f36ebcc4dd41be02d6a37fef2b781b
Your Name <you@example.com>
2020-05-14 13:21:53 +0300

    konec

--- Summ.pas
+++ Summ.pas
@@ -1,5 +1,5 @@
 ﻿var 
-  a,b,c,d:integer;
+  a,b,c,d,e:integer;
   begin
     writeln('Введите 1 число ');
     readln(a);
@@ -9,5 +9,7 @@
     writeln('Сумма = ',c);
     d:=a*b;
     writeln('Произведение = ',d);
+    e:=(a*b)-(a+b;
+    writeln('Вот это  = ',e);
    
   end.
